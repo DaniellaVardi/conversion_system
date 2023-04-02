8427131afbdc11394c37b73e19dd2182bf827586
DaniellaVardi <105429946+DaniellaVardi@users.noreply.github.com>
2023-04-02 17:15:06 +0300

    final project

--- CSVParser.py
+++ CSVParser.py
@@ -4,17 +4,51 @@ from datetime import datetime, timedelta
 
 EUR_TO_USD = 2
 AUD_TO_USD = 3
-DIVIDE_IN_100 = 100
+DIVIDE_BY_HUNDRED = 100
+DIVIDE_BY_THOUSAND = 1000
 
 def getDataFromCSV(filenameToJson, filenameSearch):
+    """
+    Reads two CSV files and returns the dataframes for converting orders to JSON and for searching reason codes.
+
+    Args:
+        filenameToJson (str): A string that represents the file path to the CSV file containing the order data for conversion.
+        filenameSearch (str): A string that represents the file path to the CSV file containing the reason code data for searching.
+
+    Returns:
+        df_toJSON (pandas.DataFrame): A pandas dataframe that contains the order data for conversion to JSON format.
+        df_toSearch (pandas.DataFrame): A pandas dataframe that contains the reason code data for searching.
+        MerchantName (str): A string that represents the name of the merchant for the order data.
+
+    Raises:
+        FileNotFoundError: If the specified file path does not exist.
+    """
     df_toJSON = pd.read_csv(filenameToJson)
     df_toSearch = pd.read_csv(filenameSearch)
-    ## TODO explain
+
+    # This line replaces the string 'AMERICAN_EXPRESS' with 'AMEX' in the DataFrame df_toSearch.
+    # This is done to match the appropriate processor name with the template defined in the file file1.
     df_toSearch = df_toSearch.replace(['AMERICAN_EXPRESS'], 'AMEX')
+
     MerchantName= df_toJSON['MerchantName'].head(1).values[0]
     return df_toJSON, df_toSearch, MerchantName
 
 def formatDateType(processor, merchantName, dateOrder, addDays=0):
+    """
+   Formats the dateOrder string based on the specified processor and merchantName.
+
+   Args:
+       processor (str): The payment processor used for the order.
+       merchantName (str): The name of the merchant that processed the order.
+       dateOrder (str): The original date of the order.
+       addDays (int, optional): The number of days to add to the date. Defaults to 0.
+
+   Returns:
+       str: The formatted date string.
+
+   Raises:
+       None
+   """
     if(processor == 'VISA'):
         if merchantName in ['MyBook', 'MyShop']:
             dateOrder_N = datetime.strptime(dateOrder, '%Y-%m-%d')
@@ -37,72 +71,139 @@ def formatDateType(processor, merchantName, dateOrder, addDays=0):
 
     return dateOrder_N
 
+def getReasonCategory(df_toSearch, reasonCode, processorName):
+    """
+    Returns the reason category associated with the given reason code and processor name, by searching in the
+    provided DataFrame df_toSearch.
+
+    Parameters:
+    - df_toSearch (pandas.DataFrame): A DataFrame containing the reason codes and categories to be searched.
+    - reasonCode (str): The reason code to be searched for.
+    - processorName (str): The name of the processor to be searched for.
+
+    Returns:
+    - str: The reason category associated with the given reason code and processor name, as a string.
+
+    Raises:
+    - IndexError: If no match is found in the DataFrame, i.e., no row has both the given reason code and processor name.
+    """
+    return df_toSearch.loc[(df_toSearch['ReasonCode'] == reasonCode) & (df_toSearch['Processor'] == processorName)]['Reasoncategory'].values[0]
+
+def createOrder(row, ordersId, df_toSearch):
+    """
+    Create a new order dictionary based on the input row and add its order ID to the ordersId list.
+
+    Args:
+    - row: a pandas Series object representing a single row in the input data
+    - ordersId: a list of order IDs to which the new order ID will be added
+    - df_toSearch: a pandas DataFrame containing information for mapping ReasonCodes to ReasonCategories
+
+    Returns:
+    - new_order: a dictionary representing the new order
+    """
+    processor = getReasonCategory(df_toSearch, row.ReasonCode, row.ProcessorName)
+    new_order = {
+        'OrderID': row.OrderID,
+        'ReasonCode':  isInt(row.ReasonCode),
+        'Amount': int(row.Amount),
+        'Currency': row.Currency,
+        'ProcessorName': row.ProcessorName,
+        'DeliveryDate': formatDateType(row.ProcessorName, row.MerchantName, row.DeliveryDate),
+        'OrderDate': formatDateType(row.ProcessorName, row.MerchantName, row.OrderDate),
+        'MerchantName': row.MerchantName,
+        'Address': row.Address,
+        'ReasonCatadory': processor,
+        'AmountUSD': int(calculate_amount_usd(row.Amount, row.Currency, row.MerchantName, row.ProcessorName)),
+        "ProcessingDate": formatDateType(row.ProcessorName, row.MerchantName, row.OrderDate, 3)
+    }
+    ordersId.append(row.OrderID)
+    return new_order
+
+def createDuplicateOrder(row, df_toSearch):
+    """
+    Creates a new order dictionary from a given row of data, with the same data as the original order
+    but with a new order ID.
+
+    Args:
+        row (pandas.Series): A pandas Series object representing a row of order data.
+        df_toSearch (pandas.DataFrame): A pandas DataFrame object representing a lookup table of processor
+            and merchant name combinations and their respective date formats.
+
+    Returns:
+        dict: A dictionary representing the new order with a duplicate of the original data but with a new order ID.
+    """
+    processor = getReasonCategory(df_toSearch, row.ReasonCode, row.ProcessorName)
+    new_order = {
+        'OrderID': row.OrderID,
+        'ReasonCode':  isInt(row.ReasonCode),
+        'Amount': int(row.Amount),
+        'Currency': row.Currency,
+        'ProcessorName': row.ProcessorName,
+        'DeliveryDate': formatDateType(row.ProcessorName, row.MerchantName, row.DeliveryDate),
+        'OrderDate': formatDateType(row.ProcessorName, row.MerchantName, row.OrderDate),
+        'MerchantName': row.MerchantName,
+        'Address': row.Address,
+        'ReasonCatadory': processor,
+        'AmountUSD': int(calculate_amount_usd(row.Amount, row.Currency, row.MerchantName, row.ProcessorName)),
+        "ProcessingDate": formatDateType(row.ProcessorName, row.MerchantName, row.OrderDate, 3)
+    }
+    return new_order
+
 def formatDataToJson_CODE(df_toJSON, df_toSearch):
+    """
+    Convert a pandas DataFrame `df_toJSON` to JSON, checking for duplicate orders.
+
+    Parameters:
+    -----------
+    df_toJSON : pandas.DataFrame
+        The DataFrame to be converted to JSON.
+    df_toSearch : pandas.DataFrame
+        The DataFrame to search for duplicate orders.
+
+    Returns:
+    --------
+    tuple : Two JSON-formatted strings:
+        - Unique orders converted from `df_toJSON`.
+        - Duplicate orders converted from `df_toJSON`.
+    """
     orders_dict =[]
     orders_dict_duplicate =[]
     ordersId = []
 
     for index, row in df_toJSON.iterrows():
 
-        processor = df_toSearch.loc[(df_toSearch['ReasonCode'] == row.ReasonCode) & (df_toSearch['Processor'] == row.ProcessorName)]['Reasoncategory'].values[0]
-
-        existsID = False
-        for id in ordersId:
-            if id == row.OrderID:
-                existsID = True
-        if existsID == True:
-            new_dict_duplicate = {
-                'OrderID': row.OrderID,
-                'ReasonCode':  row.ReasonCode,
-                'Amount': int(row.Amount),
-                'Currency': row.Currency,
-                'ProcessorName': row.ProcessorName,
-                'DeliveryDate': formatDateType(row.ProcessorName, row.MerchantName, row.DeliveryDate),
-                'OrderDate': formatDateType(row.ProcessorName, row.MerchantName, row.OrderDate),
-                'MerchantName': row.MerchantName,
-                'Address': row.Address,
-                'ReasonCatadory': processor,
-                'AmountUSD': int(calculate_amount_usd(row.Amount, row.Currency, row.MerchantName, row.ProcessorName)),
-                "ProcessingDate": formatDateType(row.ProcessorName, row.MerchantName, row.OrderDate, 3)
-            }
+        existsID = row.OrderID in ordersId
+        if existsID:
+            new_dict_duplicate = createDuplicateOrder(row, df_toSearch)
             orders_dict_duplicate.append(new_dict_duplicate)
             continue
 
-        ordersId.append(row.OrderID)
-
-        new_dict = {
-            'OrderID': row.OrderID,
-            'ReasonCode':  row.ReasonCode,
-            'Amount': int(row.Amount),
-            'Currency': row.Currency,
-            'ProcessorName': row.ProcessorName,
-            'DeliveryDate': formatDateType(row.ProcessorName, row.MerchantName, row.DeliveryDate),
-            'OrderDate': formatDateType(row.ProcessorName, row.MerchantName, row.OrderDate),
-            'MerchantName': row.MerchantName,
-            'Address': row.Address,
-            'ReasonCatadory': processor,
-            'AmountUSD': int(calculate_amount_usd(row.Amount, row.Currency, row.MerchantName, row.ProcessorName)),
-            "ProcessingDate": formatDateType(row.ProcessorName, row.MerchantName, row.OrderDate, 3)
-        }
+        new_dict = createOrder(row, ordersId, df_toSearch)
         orders_dict.append(new_dict)
-    orders_dict = json.dumps(orders_dict, indent = 4)
-    orders_dict_duplicate = json.dumps(orders_dict_duplicate, indent = 4)
+
+    orders_dict = json.dumps(orders_dict, indent=4)
+    orders_dict_duplicate = json.dumps(orders_dict_duplicate, indent=4)
 
     return orders_dict, orders_dict_duplicate
 
-
 def calculate_amount_usd(amount, currency, merchantName, processor):
-    """
-    Convert the given amount to USD based on the currency.
 
-    Args:
-        amount (float): A float representing the amount.
-        currency (str): A string representing the currency.
+    """
+    Converts `amount` from `currency` to USD, adjusting for merchant and processor rules.
+
+    Parameters:
+    amount : float
+        The amount of money to be converted.
+    currency : str
+        The currency of the original `amount`.
+    merchantName : str
+        The name of the merchant for which the `amount` was charged.
+    processor : str
+        The name of the processor that handled the transaction.
 
     Returns:
-        float: A float representing the converted amount in USD.
+    float : The equivalent `amount` in USD, after any conversions or adjustments.
     """
-
     if currency == 'EUR':
         return amount / EUR_TO_USD
     elif currency == 'AUD':
@@ -112,12 +213,27 @@ def calculate_amount_usd(amount, currency, merchantName, processor):
 
     if(processor == 'VISA'):
         if merchantName in ['MyBook', 'MyFlight']:
-            amountUSD = amountUSD / 100
+            amountUSD = amountUSD / DIVIDE_BY_HUNDRED
         elif merchantName in ['MyShop']:
-            amountUSD = amountUSD / 1000
+            amountUSD = amountUSD / DIVIDE_BY_THOUSAND
 
     return amountUSD
 
 def createOutput(orders_dict, path):
+    """
+    Write the JSON-formatted `orders_dict` to the specified `path`.
+
+    Parameters:
+    -----------
+    orders_dict : str
+        A JSON-formatted string to be written to the output file.
+    path : str
+        The file path where the output should be written.
+    """
     outFile =  open(path, "w")
     outFile.write(orders_dict)
+
+def isInt(reasonCode):
+    if reasonCode % 1 != 0:
+        return reasonCode
+    return int(reasonCode)
--- Main.py
+++ Main.py
@@ -3,13 +3,32 @@ from CSVParser import *
 
 class Main:
 
-    inputPath = '/conversion_system/Inputs/MyShop/file1.csv'
+    MAX_ATTEMPTS = 5
+    attempt_count = 0
 
+    while True:
+        inputPath = input("Please enter the file path: ") # for example /conversion_system/Inputs/MyShop/file1.csv
+        try:
+            convertedOrders, reasonCodes, MerchantName = getDataFromCSV('..'+ inputPath, "reason_codes.csv" )
+            orders_dict, orders_dict_duplicate = formatDataToJson_CODE(convertedOrders, reasonCodes)
+            outputPath = inputPath.replace('Inputs', 'Outputs').replace('.csv', '')
 
-    convertedOrders, reasonCodes, MerchantName = getDataFromCSV('..'+ inputPath, "reason_codes.csv" )
-    orders_dict, orders_dict_duplicate = formatDataToJson_CODE(convertedOrders, reasonCodes)
-    outputPath = inputPath.replace('Inputs', 'Outputs').replace('.csv', '')
+            createOutput(orders_dict, '..' + outputPath +'.json')
+            createOutput(orders_dict_duplicate, '..' + outputPath+'_Duplicate.json')
 
-    createOutput(orders_dict, '..' + outputPath +'.json')
-    createOutput(orders_dict_duplicate, '..' + outputPath+'_Duplicate.json')
+            print("The Converted Orders JSON file located at:")
+            print(outputPath + '.json')
+            print("\n")
+            print("The Converted Duplicate Orders JSON file located at:")
+            print(outputPath + '_Duplicate.json')
+            break
 
+        except FileNotFoundError:
+            attempt_count += 1
+            if attempt_count < MAX_ATTEMPTS:
+                print("Error: The specified file was not found. Please try again.")
+            else:
+                print("Error: The specified file was not found. Maximum number of attempts reached.")
+                break
+        except Exception as e:
+            print(f"Error: {e}")
